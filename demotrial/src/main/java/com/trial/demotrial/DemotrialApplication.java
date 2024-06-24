9c2e087a97a805d4bfb84e9fd7b5d510cad5475d
Darinc <boyomnzussouodaringsteve@gmail.com>
2024-06-24 10:37:03 +0100

    student mangement system

---
+++ demotrial/src/main/java/com/trial/demotrial/DemotrialApplication.java
@@ -0,0 +1,13 @@
+package com.trial.demotrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotrialApplication.class, args);
+	}
+
+}
---
+++ demotrial/src/test/java/com/trial/demotrial/DemotrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trial.demotrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
